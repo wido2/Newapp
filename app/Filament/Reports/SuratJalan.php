c4ec503df6076cbfff85b2aa26a2e975585df5e5
wido2 <wid.o2@live.com>
2024-09-03 14:13:38 +0700

    link storage

--- app/Filament/Reports/SuratJalan.php
+++ app/Filament/Reports/SuratJalan.php
@@ -7,14 +7,19 @@ use App\Models\Produk;
 use Filament\Forms\Form;
 use EightyNine\Reports\Report;
 use Illuminate\Support\Collection;
-use EightyNine\Reports\Components\Body;
-use EightyNine\Reports\Components\Footer;
+use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Select;
 // use Illuminate\Database\Eloquent\Collection;
+use EightyNine\Reports\Components\Body;
+use EightyNine\Reports\Components\Text;
+use EightyNine\Reports\Components\Image;
+use EightyNine\Reports\Components\Footer;
 use EightyNine\Reports\Components\Header;
 use EightyNine\Reports\Components\Body\Table;
 use App\Models\SuratJalan as ModelsSuratJalan;
 use EightyNine\Reports\Components\Body\TextColumn;
-use Filament\Forms\Components\Select;
+use EightyNine\Reports\Components\Header\Layout\HeaderRow;
+use EightyNine\Reports\Components\Header\Layout\HeaderColumn;
 
 class SuratJalan extends Report
 {
@@ -24,13 +29,41 @@ class SuratJalan extends Report
 
     public function header(Header $header): Header
     {
+      $k=Auth::user();
+        $imgpath='/public/storage/logo.png';
         return $header
             ->schema([
-                // ...
+                HeaderRow::make()
+                ->schema([
+
+               HeaderColumn::make()
+               ->schema([
+                    Image::make('/storage/logo.png')
+                    ->width2Xl()
+                    ,
+                    Text::make('Report Surat Jalan')
+                        // ->size('large')
+                        // ->weight('bold')
+                        ->color('primary'),
+                    Text::make('Tanggal : '.date('d-m-Y'))
+                        // ->size('small')
+                    // ->weight('thin')     
+                ])->alignLeft(),
+                HeaderColumn::make()
+                ->schema([
+                    Text::make('Dibuat Oleh : '. $k->name)
+                        // ->size('small')
+                        // ->weight('thin')
+                        ,
+                    // Text::make('Dibuat Tanggal : '.date('d-m-Y H:i:s'))
+                ])->alignRight()
+                ])
+
             ]);
+
     }
 
-
+    
     public function body(Body $body): Body
     {
         // $dataku=ModelsSuratJalan::with('produk')->where('nomor_surat_jalan','=','1');
@@ -88,6 +121,10 @@ class SuratJalan extends Report
                 Select::make('nomor_surat_jalan')
                 ->searchable()
                 ->preload()
+                ->optionsLimit(1)
+                ->searchingMessage('Mencari...')
+                ->noSearchResultsMessage('Nomor tidak ditemukan')
+                ->searchPrompt('Nomor Surat Jalan')
                 ->options(
                     ModelsSuratJalan::all()
                     ->pluck('nomor_surat_jalan', 'id')
