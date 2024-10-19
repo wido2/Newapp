bdb1f928cb860e589ee0e080c71a90bd329e7bc2
wido2 <wid.o2@live.com>
2024-10-19 11:33:06 +0700

    !

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '4d8fe364a9090f45256ca22e9e8573befd7cab7c',
+        'reference' => 'b6d613c6e9baeed8b60a57065bccabe6f438e2a2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -613,7 +613,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '4d8fe364a9090f45256ca22e9e8573befd7cab7c',
+            'reference' => 'b6d613c6e9baeed8b60a57065bccabe6f438e2a2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -1159,8 +1159,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
